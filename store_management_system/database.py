a6c39c72422fc6b77b57683f18bdafdde8ae0ba6
shubh220922 <shubh22.srivastava@gmail.com>
2023-10-13 13:24:12 +0300

    (commit 5) new UI, added support for store outlet service

--- store_management_system/database.py
+++ store_management_system/database.py
@@ -1,19 +1,13 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, Column, String, Integer, select
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine, Column, String, Integer, select, ForeignKey, Float
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 from sqlalchemy.exc import IntegrityError
 import bcrypt
 
 
 load_dotenv()
 Base = declarative_base()
-
-engine = create_engine(os.getenv("DB_STRING"), echo=True)
-Base.metadata.create_all(bind=engine)  # takes all the classes and creates their database
-
-Session = sessionmaker(bind=engine)
-session = Session()  # we can do all sort of things with this now
 
 
 class Owners(Base):
@@ -23,6 +17,7 @@ class Owners(Base):
     name = Column("name", String(50), nullable=False)
     email = Column("email", String(100), nullable=False, unique=True)
     password = Column("password", String(100), nullable=False)
+    outlets = relationship('Outlets', back_populates='owner')
 
     def __init__(self, name, email, password):
         self.name = name
@@ -31,6 +26,26 @@ class Owners(Base):
 
     def __repr__(self):
         return f"Name: {self.name}\n email: {self.email}"
+
+
+class Outlets(Base):
+    __tablename__ = "outlets"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    lat = Column(Float, nullable=False)
+    lng = Column(Float, nullable=False)
+    owner_id = Column(Integer, ForeignKey('owners.id'))
+    owner = relationship('Owners', back_populates='outlets')
+
+    def __repr__(self):
+        return f"<Outlet(outlet_id={self.outlet_id}, lat={self.lat}, lng={self.lng}, owner_id={self.owner_id})>"
+
+
+engine = create_engine(os.getenv("DB_STRING"), echo=True)
+
+Session = sessionmaker(bind=engine)
+session = Session()  # we can do all sort of things with this now
+Base.metadata.create_all(bind=engine)  # takes all the classes and creates their database
 
 
 def insert_owner_into_db(details):
@@ -69,6 +84,53 @@ def verify_signin_with_db(details):
         if bcrypt.checkpw(user_password, password):
             row = result[0]
             name = row.name
-            return name
+            id = row.id
+            return id, name
 
     return None
+
+
+def retrieve_store_loc(owner_id):
+    try:
+        get_store_list = session.query(Outlets).filter(Outlets.owner_id == owner_id).all()
+        session.flush()
+        response = []
+
+        for store in get_store_list:
+            response.append({
+                'lat': store.lat,
+                'lng': store.lng
+            })
+        return response
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+def add_store_loc(store_owner_id, store_lat, store_lng):
+    try:
+        new_loc = Outlets(lat=store_lat, lng=store_lng, owner_id=store_owner_id)
+        session.add(new_loc)
+        session.commit()
+        return True
+    except IntegrityError:
+        session.rollback()
+        print("\033[91mError while inserting store location\033[0m")
+        return False
+
+
+def get_employees(owner_id):
+    try:
+        get_employee_list = session.query(employee).all()
+        session.flush()
+        response = []
+
+        for store in get_store_list:
+            response.append({
+                'lat': store.lat,
+                'lng': store.lng
+            })
+        return response
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
--- store_management_system/server.py
+++ store_management_system/server.py
@@ -1,6 +1,6 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
-from database import insert_owner_into_db, check_presence_in_db, verify_signin_with_db
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from database import insert_owner_into_db, check_presence_in_db, verify_signin_with_db, retrieve_store_loc, add_store_loc
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
@@ -28,16 +28,16 @@ def sign_in():
 @app.route("/verify_signin", methods=['POST'])
 def verify_signin():
     data = request.form
-    user_name = verify_signin_with_db(data)
-    if user_name is not None:
-        return redirect(url_for('user_dashboard', name=user_name))
+    user_id, user_name = verify_signin_with_db(data)
+    if user_id is not None:
+        return redirect(url_for('user_dashboard', id=user_id, name=user_name))
     else:
         return render_template('user_does_not_exist.html')
 
 
-@app.route("/<name>/dashboard")
-def user_dashboard(name):
-    return render_template('dashboard.html', details={'name': name})
+@app.route("/<id>/<name>/dashboard")
+def user_dashboard(id, name):
+    return render_template('dashboard.html', details={'owner_id': id, 'name': name})
 
 
 @app.route("/signup/successful", methods=['POST'])
@@ -52,6 +52,98 @@ def signup_successful():
             return "<h1>User already exists</h1>"
         return render_template('signup_successful.html',
                                signup_details=data)
+
+
+@app.route("/<id>/<name>/outlet")
+def outlet(id, name):
+
+    return render_template('outlet.html',
+                           details={'owner_id': id, 'name': name})
+
+
+@app.route("/<id>/<name>/employee-data")
+def employee_data(id, name):
+    db = [
+        {
+            'id': 1,
+            'name': "kashyap",
+            'post': "receptionist",
+            'salary': 1200
+        },
+        {
+            'id': 2,
+            'name': "ashwin",
+            'post': "janitor",
+            'salary': 800
+        }
+    ]
+
+    return render_template('employee_data.html',
+                           details={'owner_id': id, 'name': name},
+                           emp_data=db)
+
+
+@app.route("/<id>/<name>/issue-order")
+def issue_order(id, name):
+    db = [
+        {
+            'order_id': 1,
+            'products': [{
+                             'prod_id': 1,
+                             'prod_name': "Shaving cream"
+                         },
+                         {
+                             'prod_id': 2,
+                             'prod_name': "Detergent"
+                         },
+                         {
+                             'prod_id': 3,
+                             'prod_name': "Vegetable oil"
+                         }]
+        }
+    ]
+
+    return render_template('issue_order.html',
+                           details={'owner_id': id, 'name': name},
+                           order_data=db)
+
+
+@app.route("/get_coordinates/<owner_id>")
+def get_coordinates(owner_id):
+    coords = retrieve_store_loc(owner_id)
+
+    print(coords)
+
+    if coords is not None:
+        return jsonify({
+            "owner_id": owner_id,
+            "coordinates": coords
+        })
+    else:
+        return jsonify([])
+
+
+@app.route("/<id>/<name>/add_loc", methods=['POST'])
+def add_loc(id, name):
+    data = request.form
+
+    lat = data.get('latitude')
+    lng = data.get('longitude')
+
+    if not lat or not lng:
+        return render_template('outlet.html',
+                               details={'owner_id': id, 'name': name},
+                               message="Latitude and Longitude fields cannot be null")
+    else:
+        status = add_store_loc(id, lat, lng)
+
+        if status:
+            return render_template('outlet.html',
+                                   details={'owner_id': id, 'name': name})
+        else:
+            return render_template('outlet.html',
+                                   details={'owner_id': id, 'name': name},
+                                   message="There was an error inserting the store location")
 
 
 if __name__ == "__main__":
